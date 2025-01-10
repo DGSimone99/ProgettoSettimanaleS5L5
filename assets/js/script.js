b20eb76446ce4b84544e99068e67355425446fae
DGSimone99 <simone.digiorgio99@gmail.com>
2025-01-10 21:09:22 +0100

    Esame 10-01-2025

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let nav = document.querySelector("nav");
+  let button = document.querySelector(".button");
+  let hero = document.querySelector("#hero");
+
+  window.addEventListener("scroll", () => {
+    let heroScroll = hero.getBoundingClientRect().bottom;
+
+    if (heroScroll <= 100) {
+      nav.classList.add("white");
+      button.classList.add("green");
+    } else {
+      nav.classList.remove("white");
+      button.classList.remove("green");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  let m = document.querySelectorAll('g[transform^="matrix"]');
+
+  function random() {
+    let rand = Array.from(m).sort(() => Math.random() - 0.5);
+
+    rand.forEach((mRand, index) => {
+      setTimeout(() => {
+        if (mRand.style.opacity === "0") {
+          mRand.style.opacity = "1";
+        } else {
+          mRand.style.opacity = "0";
+        }
+      }, index * 1000);
+    });
+  }
+
+  setInterval(random, 1000);
+});
+
+/* Avevo intuito correttamente che dovevo ciclare le M e cambiarne l'opacità, 
+ma ammetto di aver chiesto a ChatGPT quale parte dell'SVG doveva essere ciclato, ovvero g[transform^="matrix"]*/
